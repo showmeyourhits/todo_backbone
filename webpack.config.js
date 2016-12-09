ec125eddfc30b808dd2a603baa8790b39c06c9d7
tk_user <tk_user@dtulyakov.local>
2016-12-09 15:22:19 +0300

    boilerplate?

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: './dist'
+  },
+	devServer: {
+	    inline: true,
+	    port: 4444,
+	    contentBase: "./dist"
+	},
+  module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/, /typings/],
+                loader: "babel",
+                query: {
+                    presets: ["es2015"]
+                }
+            }
+        ]
+    }
+}
