7bf6690981e3704fce23255c1023bdb1b1a42cf3
tk_user <tk_user@dtulyakov.local>
2016-12-13 13:02:12 +0300

    moved to scss

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import TodoListView from './todolist/views/TodoListView.js';
 
-require("./todolist/style/todo_list.css");
-require("./todolist/style/todo_view.css");
-require("./todolist/style/todo_stats.css");
+require("./todolist/style/todo_list.scss");
+require("./todolist/style/todo_view.scss");
+require("./todolist/style/todo_stats.scss");
 const app = new TodoListView();
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
-  entry: './app/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: './dist'
-  },
-  devtool: "source-map",
-	devServer: {
-	    inline: true,
-	    port: 4444,
-	    contentBase: "./dist"
+	entry: './app/index.js',
+	output: {
+	filename: 'bundle.js',
+	path: './dist'
 	},
-    eslint:{
-        configFile: '.eslintrc'
-    },
-  module: {
-        preLoaders: [
-            // {
-            //     test: /\.js$/,
-            //     exclude: [/node_modules/],
-            //     loader: "eslint-loader"
-            // }
-        ],
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: [/node_modules/],
-                loader: "babel",
-                query: {
-                    presets: ["es2015"]
-                }
-            },
-            {
-                test: /\.css$/,
-                exclude: [/node_modules/],
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader"),
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin("bundle.css"),
-    ]
+	devtool: "source-map",
+	devServer: {
+		inline: true,
+		port: 4444,
+		contentBase: "./dist"
+	},
+	eslint:{
+		configFile: '.eslintrc'
+	},
+	module: {
+		preLoaders: [
+			{
+				test: /\.js$/,
+				exclude: [/node_modules/],
+				loader: "eslint-loader"
+			}
+		],
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: [/node_modules/],
+				loader: "babel",
+				query: {
+					presets: ["es2015"]
+				}
+			},
+			{
+				test: /\.scss$/,
+				exclude: [/node_modules/],
+				loader: ExtractTextPlugin.extract("style-loader", "css-loader", "sass-loader"),
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin("bundle.css"),
+	]
 }
